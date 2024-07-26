f502633aa3ddb7cbf328d4cbb36908da4531335e
indenta-ai <shahbazabdullah81@gmail.com>
2024-07-26 13:11:43 +0500

    email env updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;;
 
 app.use(cors());
 app.use(bodyParser.json());
